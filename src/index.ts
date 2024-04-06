bb90a1eb78862e04f710d81c4404d69716d05d57
pedro11pucci <pedro11pucci@gmail.com>
2024-04-06 19:05:40 -0300

    Create connection to MySQL database

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import Express, { Request, Response } from 'express';
+import appDataSource from "./infra/data-source";
 
 const app = Express()
 require('dotenv').config()
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on http://localhost:${process.env.PORT}`)
+appDataSource.initialize().then(() => {
+    console.log("Database initialized");
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on http://localhost:${process.env.PORT}`)
+    })
 })
+
 
 app.get('/', async (req, res) => {
     res.send("Hello World!")
