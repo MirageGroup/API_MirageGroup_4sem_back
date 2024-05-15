0096326d4f840a8147bb912af81926e76be46564
Gustavo Henrique <108089562+gustavohpereira@users.noreply.github.com>
2024-05-14 22:33:26 -0300

    add meeting theme on query

--- src/services/meeting.service.ts
+++ src/services/meeting.service.ts
@@ -25,7 +25,27 @@ export class MeetingServices {
         const meetings = await this.meetingRepository
         .createQueryBuilder('meeting')
         .leftJoin('meeting.participants', 'user')
+        .leftJoinAndSelect('meeting.physicalRoom', 'physicalRoom')
+        .leftJoinAndSelect('meeting.virtualRoom', 'virtualRoom')
         .where('user.id = :id', { id })
+        .select([
+            'meeting.id',
+            'meeting.protocol',
+            'meeting.description',
+            'meeting.beginning_time',
+            'meeting.end_time',
+            'meeting.meetingType',
+            'physicalRoom.id',
+            'physicalRoom.name',
+            'physicalRoom.description',
+            'physicalRoom.occupancy',
+            'physicalRoom.location',
+            'physicalRoom.accessLevel',
+            'virtualRoom.id',
+            'virtualRoom.login',
+            'virtualRoom.accessLevel',
+            'meeting.meetingTheme'
+        ])
         .getMany()
         if(meetings.length === 0) throw new FindRelationsNotFoundError(['participants'])
         return meetings
