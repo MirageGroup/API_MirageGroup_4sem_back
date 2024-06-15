93d4f37c78e5c3c349aa4e08ebcbdb0c99e43353
pedro11pucci <pedro11pucci@gmail.com>
2024-06-15 16:36:31 -0300

    fix: email address

--- src/Data/SendEmail.ts
+++ src/Data/SendEmail.ts
@@ -1,26 +1,28 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
-
+dotenv.config();
 
 export default async function SendEmail(destinatario: string, assunto: string, corpo: string) {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.TRANSPORTER_EMAIL,
-          pass: process.env.TRANSPORTER_PASSWORD,
-        },
+  const transporter = nodemailer.createTransport({
+      service: 'hotmail',
+      auth: {
+        user: process.env.TRANSPORTER_EMAIL,
+        pass: process.env.TRANSPORTER_PASSWORD,
+      },
+    });
+
+  try {
+      // Enviando o email
+      console.log(destinatario)
+      await transporter.sendMail({
+        from: process.env.TRANSPORTER_EMAIL, // Remover as aspas para usar a variável corretamente
+        to: destinatario,
+        subject: assunto,
+        text: corpo,
       });
-    try {
-        // Enviando o email
-        await transporter.sendMail({
-          from: 'gustavohpa2003@gmail.com',
-          to: destinatario,
-          subject: assunto,
-          text: corpo,
-        });
-        console.log('Email enviado com sucesso!');
-      } catch (error) {
-        console.error('Erro ao enviar o email:', error);
-      }
+      console.log('Email enviado com sucesso!');
+    } catch (error) {
+      console.error('Erro ao enviar o email:', error);
+    }
 }
